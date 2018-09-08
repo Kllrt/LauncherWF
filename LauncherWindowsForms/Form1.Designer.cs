f35d95b75160cf0133e8cecb77d694e3d9879c81
Kllrt <kllrtik@gmail.com>
2018-09-08 09:17:54 +0200

    Ayy lmao

--- LauncherWindowsForms/Form1.Designer.cs
+++ LauncherWindowsForms/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LauncherWindowsForms
 {
-    partial class Form1
+    partial class Launcher
     {
         /// <summary>
         /// Vyžaduje se proměnná návrháře.
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Launcher));
+            this.LaunchGame = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.AddonList = new System.Windows.Forms.CheckedListBox();
+            this.FilePatching = new System.Windows.Forms.CheckBox();
+            this.SkipIntro = new System.Windows.Forms.CheckBox();
+            this.CustomParams = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LaunchGame
+            // 
+            resources.ApplyResources(this.LaunchGame, "LaunchGame");
+            this.LaunchGame.Name = "LaunchGame";
+            this.LaunchGame.UseVisualStyleBackColor = true;
+            this.LaunchGame.Click += new System.EventHandler(this.LaunchGame_Click);
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // AddonList
+            // 
+            resources.ApplyResources(this.AddonList, "AddonList");
+            this.AddonList.FormattingEnabled = true;
+            this.AddonList.Name = "AddonList";
+            this.AddonList.SelectedIndexChanged += new System.EventHandler(this.AddonList_SelectedIndexChanged);
+            // 
+            // FilePatching
+            // 
+            resources.ApplyResources(this.FilePatching, "FilePatching");
+            this.FilePatching.Name = "FilePatching";
+            this.FilePatching.UseVisualStyleBackColor = true;
+            this.FilePatching.CheckedChanged += new System.EventHandler(this.FilePatching_CheckedChanged);
+            // 
+            // SkipIntro
+            // 
+            resources.ApplyResources(this.SkipIntro, "SkipIntro");
+            this.SkipIntro.Name = "SkipIntro";
+            this.SkipIntro.UseVisualStyleBackColor = true;
+            this.SkipIntro.CheckedChanged += new System.EventHandler(this.SkipIntro_CheckedChanged);
+            // 
+            // CustomParams
+            // 
+            resources.ApplyResources(this.CustomParams, "CustomParams");
+            this.CustomParams.Name = "CustomParams";
+            this.CustomParams.TextChanged += new System.EventHandler(this.CustomParams_TextChanged);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Launcher
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CustomParams);
+            this.Controls.Add(this.SkipIntro);
+            this.Controls.Add(this.FilePatching);
+            this.Controls.Add(this.AddonList);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.LaunchGame);
+            this.Name = "Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button LaunchGame;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckedListBox AddonList;
+        private System.Windows.Forms.CheckBox FilePatching;
+        private System.Windows.Forms.CheckBox SkipIntro;
+        private System.Windows.Forms.TextBox CustomParams;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- LauncherWindowsForms/Form1.cs
+++ LauncherWindowsForms/Form1.cs
@@ -10,11 +10,119 @@ using System.Windows.Forms;
 
 namespace LauncherWindowsForms
 {
-    public partial class Form1 : Form
+    public partial class Launcher : Form
     {
-        public Form1()
+        private string arguments = null;
+        private string finalarguments = null;
+        private string fp = null;
+        private string customprms = null;
+        private string mods = null;
+
+        public Launcher()
         {
             InitializeComponent();
+            LoadAddons();
+        }
+
+        private void FilePatching_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void AddonList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void CustomParams_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            arguments += " -nosplash";
+        }
+
+        private void SkipIntro_CheckedChanged(object sender, EventArgs e)
+        {
+            arguments += " -skipintro";
+        }
+
+        public void CheckForFilePatching()
+        {
+            fp = null;
+            if (FilePatching.Checked == true)
+            {
+                fp += " -FilePatching";
+            }
+            else if (!FilePatching.Checked == true)
+            {
+                fp += " -noFilePatching";
+            }
+        }
+
+        public void CustomParamsGet()
+        {
+            customprms = null;
+            customprms += " ";
+            customprms += CustomParams.Text;
+        }
+
+        public void LoadAddons()
+        {
+            try
+            {
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(@"d:\\Games\\Steam\\steamapps\\common\\Arma 3\\list.txt"))
+                {
+                    AddonList.Items.Clear();
+                    while (!sr.EndOfStream)
+                    {
+                        for (int i = 0; i < 4; i++)
+                        {
+                            string strListItem = sr.ReadLine();
+                            if (!String.IsNullOrEmpty(strListItem))
+                                AddonList.Items.Add(strListItem);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public void  CheckAddonsList()
+        {
+            mods = "-mod=";
+            // Next show the object title and check state for each item selected.
+            foreach (object itemChecked in AddonList.CheckedItems)
+            {
+                // Use the IndexOf method to get the index of an item.
+                mods += itemChecked.ToString();
+                mods += ";";
+                //System.IO.Directory.GetDirectories;
+            }
+        }
+        private void LaunchGame_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                CheckAddonsList();
+                CheckForFilePatching();
+                CustomParamsGet();
+                finalarguments = mods + arguments + fp + customprms;
+                System.Diagnostics.Process.Start("D:\\Games\\Steam\\steamapps\\common\\Arma 3\\arma3_x64.exe", finalarguments);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
         }
     }
 }
